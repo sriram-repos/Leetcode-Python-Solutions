dbe6fdd1c44d00ca8a8c901584e159d2028796da
Sriram Krishnamurthy <115957733+sriram-repos@users.noreply.github.com>
2026-09-02 16:58:23 +1000

    Update solution.py

--- Easy/0001-two-sum/solution.py
+++ Easy/0001-two-sum/solution.py
@@ -1,1 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Store numbers already searched along with it's index
+        dict_searched = {}
+
+        # Search the input list of numbers
+        for i, val in enumerate(nums):
+            # Check if this the value we are searching for
+            pair_val = target - val
+
+            # Store in the dictionary once searched. If pair found return the value from dictionary
+            if pair_val in dict_searched:
+                # return the value
+                return [
+                    dict_searched[pair_val],
+                    i,
+                ]  # value and it's position in the dictionary
+
+            # if the pair value is not found store it in dictionary
+            dict_searched[val] = i
 
