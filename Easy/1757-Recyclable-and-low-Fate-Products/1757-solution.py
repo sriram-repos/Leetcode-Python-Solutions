4aa1514271f37f91dc47d15c45db7fe1719c29ec
Sriram Krishnamurthy <115957733+sriram-repos@users.noreply.github.com>
2026-09-03 19:35:01 +1000

    PySpark solution to the 1757 Leetcode problem
    PySpark solution to the 1757 Leetcode problem

--- Easy/1757-Recyclable-and-low-Fate-Products/1757-solution.py
+++ Easy/1757-Recyclable-and-low-Fate-Products/1757-solution.py
@@ -1,1 +1,26 @@
+#----------------------------------------#
+#Leetcode 1757 solution using PySpark
+#SQL to pySpark
+#----------------------------------------#
 
+# 1. Define data (Products table)
+import pandas as pd
+data = [(0,'Y','N'), (1,'Y','Y'),(2,'N','Y'),(3,'Y','Y'),(4,'N','N')]
+
+# 2. Define schema types
+products = pd.DataFrame(data, columns=['product_id', 'low_fats', 'recyclable']).astype(
+    {'product_id': 'int64', 'low_fats': 'category', 'recyclable': 'category'})
+
+#Spark Session
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+
+# 3. Create Spark dataframe
+prod_sp_df = spark.createDataFrame(products)
+##display(prod_sp_df)
+
+# 4. Filtered dataframe
+prod_sp_df.createOrReplaceTempView('Products')
+spark.sql('''
+select product_id from products where low_fats="Y" and recyclable="Y";
+''').show()
